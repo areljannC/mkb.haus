cb72f3c0debaecb269b3e04c1415e43c6ebaf9f7
AJ Clemente <areljann.c@gmail.com>
2020-11-25 21:25:56 -0500

    DEVELOP: Add login page

---
+++ client/src/pages/login.js
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+const LoginPage = () => (
+  <>
+    <Head>
+      <title>Login</title>
+    </Head>
+    <div>
+      login page
+    </div>
+  </>
+)
+
+export default LoginPage
